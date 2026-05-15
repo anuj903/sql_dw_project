aa998c5c478a13d0dcb362ebdaa4a26985aedeac
anuj903 <anujgaikwad392@gmail.com>
2026-05-15 16:07:18 +0530

    bronze layer

---
+++ scripts/Bronze_Layer/data_load_stored_procedure.sql
@@ -0,0 +1,85 @@
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	--FULL LOAD
+	PRINT '===============================================';
+	PRINT 'Loading Bronze Layer';
+	PRINT '===============================================';
+
+	PRINT '-----------------------------------------------';
+	PRINT 'Loading CRM Tables';
+	PRINT '-----------------------------------------------';
+
+	--1
+	PRINT '>> Truncating table: bronze.crm_cust_info';
+	TRUNCATE TABLE bronze.crm_cust_info;
+
+	PRINT '>> Inserting Data Into : bronze.crm_cust_info';
+	BULK INSERT bronze.crm_cust_info
+	FROM 'D:\Work\WORK__INDI4 Consultants Private Ltd\Courses\SQL Projects\SQL project 1 -Data warehouse\datasets\source_crm\cust_info.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	--2
+	PRINT '>> Truncating table: bronze.crm_prd_info';
+	TRUNCATE TABLE bronze.crm_prd_info;
+	PRINT '>> Inserting Data Into : bronze.crm_prd_info';
+	BULK INSERT bronze.crm_cust_info
+	FROM 'D:\Work\WORK__INDI4 Consultants Private Ltd\Courses\SQL Projects\SQL project 1 -Data warehouse\datasets\source_crm\prd_info.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	--3
+	PRINT '>> Truncating table: bronze.crm_sales_details';
+	TRUNCATE TABLE bronze.crm_sales_details;
+	PRINT '>> Inserting Data Into : bronze.crm_sales_details';
+	BULK INSERT bronze.crm_sales_details
+	FROM 'D:\Work\WORK__INDI4 Consultants Private Ltd\Courses\SQL Projects\SQL project 1 -Data warehouse\datasets\source_crm\sales_details.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+
+	--4
+	PRINT '>> Truncating table: bronze.erp_cust_az12';
+	TRUNCATE TABLE bronze.erp_cust_az12;
+	PRINT '>> Inserting Data Into : bronze.erp_cust_az12';
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'D:\Work\WORK__INDI4 Consultants Private Ltd\Courses\SQL Projects\SQL project 1 -Data warehouse\datasets\source_erp\CUST_AZ12.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	--5
+	PRINT '>> Truncating table: bronze.erp_loc_a101';
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	PRINT '>> Inserting Data Into : bronze.erp_loc_a101';
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'D:\Work\WORK__INDI4 Consultants Private Ltd\Courses\SQL Projects\SQL project 1 -Data warehouse\datasets\source_erp\LOC_A101.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	--6
+	PRINT '>> Truncating table: bronze.erp_px_cat_g1v2';
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	PRINT '>> Inserting Data Into : bronze.erp_px_cat_g1v2';
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'D:\Work\WORK__INDI4 Consultants Private Ltd\Courses\SQL Projects\SQL project 1 -Data warehouse\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+END
