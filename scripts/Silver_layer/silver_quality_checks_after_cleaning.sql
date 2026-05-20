484a1075d015f648780b311d66943109d0ebdbcb
anuj903 <anujgaikwad392@gmail.com>
2026-05-20 22:10:53 +0530

    silver layer cleaning

---
+++ scripts/Silver_layer/silver_quality_checks_after_cleaning.sql
@@ -0,0 +1,31 @@
+select * FROM silver.crm_cust_info;
+
+-- Check for nulls or duplicates in Primary KEY
+-- Expectation - No Results
+
+SELECT cst_id, count(*)
+FROM silver.crm_cust_info
+group by cst_id
+having count(*) > 1 -- OR cst_id IS NULL;
+
+
+-- CHECK for unwanted spaces
+-- Expectation - No Results
+
+SELECT 
+	cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != trim(cst_firstname);
+
+SELECT 
+	cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != trim(cst_lastname);
+
+
+-- Data Standardization & consistency 
+Select DISTINCT cst_gndr
+from silver.crm_cust_info;
+
+SELECT DISTINCT cst_material_status 
+from silver.crm_cust_info;
