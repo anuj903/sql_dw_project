68d8f5dd0dda81200901ee8e155f9812030c5b67
anuj903 <anujgaikwad392@gmail.com>
2026-05-20 11:29:02 +0530

    bronze Layer

--- scripts/Bronze_Layer/data_load_stored_procedure.sql
+++ scripts/Bronze_Layer/data_load_stored_procedure.sql
@@ -2,8 +2,9 @@ EXEC bronze.load_bronze
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	DECLARE @start_time	DATETIME, @end_time DATETIME	
+	DECLARE @start_time	DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
 	BEGIN TRY
+		SET @batch_start_time = GETDATE();
 		PRINT '===============================================';
 		PRINT 'Loading Bronze Layer';
 		PRINT '===============================================';
@@ -108,6 +109,12 @@ BEGIN
 		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'Seconds';
+		PRINT '------------------';
+
+		SET @batch_end_time = GETDATE();
+		PRINT '===========================================';
+		PRINT 'Bronze layer Loading Completed';
+		PRINT '  -  Total Load Duration ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time)AS NVARCHAR) + ' seconds';
 
 	END TRY
 	BEGIN CATCH
